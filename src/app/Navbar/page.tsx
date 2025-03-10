3aed58bae56332eac0e92406f229238423c82f6a
situ5901 <situdancer9@gmail.com>
2025-03-11 00:53:21 +0530

    Welco

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -36,7 +36,6 @@ function page() {
               <path
                 stroke="currentColor"
                 strokeLinecap="round"
-                strokeLinecap="round"
                 strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
