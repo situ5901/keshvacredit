4350609eaa2ca1de5467c0ac52c196c1832c1000
situ5901 <situdancer9@gmail.com>
2025-03-12 13:36:21 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ function page() {
   return (
     <>
 
-
 <div id="default-carousel" className="relative w-full" data-carousel="slide">
     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
