223493aa4b569892ae6741d35b6442001d4dc95d
situ5901 <situdancer9@gmail.com>
2025-03-12 13:24:01 +0530

    working6

--- src/app/Privacy/page.tsx
+++ src/app/Privacy/page.tsx
@@ -61,7 +61,7 @@ function page() {
 
           <p className="mb-4">
             A cookie is a small file that asks permission to be placed on your
-            computer's hard drive. Once you agree, the file is added, and the
+            computer&apos;s hard drive. Once you agree, the file is added, and the
             cookie helps analyze web traffic or lets you know when you visit a
             particular site. Cookies allow web applications to respond to you as
             an individual. The web application can tailor its operations to your
