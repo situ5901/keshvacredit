d3415930deb7727855d0e8184e05dc458d27814b
situ5901 <situdancer9@gmail.com>
2025-03-13 11:43:50 +0530

    under-const2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./Navbar/page";
-import Footer from "./Footer/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
