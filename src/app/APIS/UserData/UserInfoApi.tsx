9d093939f9abdf9368516a2e7c297c24e828f478
situ5901 <situdancer9@gmail.com>
2025-04-10 11:08:00 +0530

    🔥situ login api update01

--- src/app/APIS/UserData/UserInfoApi.tsx
+++ src/app/APIS/UserData/UserInfoApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://keshvacredit.com/api/v1/api";
-const API_BASE_URL = "http://localhost:5000/api/v1/api";
+const API_BASE_URL = "https://keshvacredit.com/api/v1/api";
+// const API_BASE_URL = "http://localhost:5000/api/v1/api";
 export const submitUserInfo = async (userData: object) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/userinfo`, userData, {
