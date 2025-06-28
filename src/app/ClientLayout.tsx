44f6c1c40e529de868a7c3dd8ab9711c5427d740
parveen082 <parveenchouhan082@gmail.com>
2025-06-28 10:19:01 +0530

    disabled right clickk and inspect elements

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect } from "react";
 import { ModalProvider } from "@/app/context/ModalContext";
 import Navbar from "./Navbar/page";
 import Footer from "./Footer/Footer";
@@ -7,6 +8,30 @@ import GlobalModal from "./Component/Modals/GlobalModal";
 import Bot from "./bott/page";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const disableInspect = (e: KeyboardEvent) => {
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key)) ||
+        (e.ctrlKey && e.key === "U")
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    const disableRightClick = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("keydown", disableInspect);
+    document.addEventListener("contextmenu", disableRightClick);
+
+    return () => {
+      document.removeEventListener("keydown", disableInspect);
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
+
   return (
     <ModalProvider>
       <Navbar />
